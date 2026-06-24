6dc90e031c4f6f6f431c08e3870e964cfbac6308
Thapelo Skhona Magqazana <111790122+thapelomagqazana@users.noreply.github.com>
2026-06-24 19:17:27 +0200

    feat(vehicle): define canonical car speed field (#36)
    Signed-off-by: Thapelo <tapsmcgzee8@gmail.com>

--- src/simulation/vehicle/index.ts
+++ src/simulation/vehicle/index.ts
@@ -8,9 +8,15 @@
 export type { CarDecision, CarPosition, CarState } from "./carState";
 
 export {
+  DEFAULT_CAR_MAX_REVERSE_SPEED,
+  DEFAULT_CAR_MAX_SPEED,
   DEFAULT_CAR_POSITION,
+  DEFAULT_CAR_SPEED,
   DEFAULT_CAR_STATE,
+  clampCarSpeed,
   createCarPosition,
   createInitialCarState,
   isValidCanvasPositionValue,
+  isValidCarSpeedValue,
+  isValidSpeedLimit,
 } from "./carState";
